2917a87136e6377a753a2a53dd0d8dde7b4bf3e7
Mohamed Abdullah <mas_seyam@yahoo.com>
2020-04-27 17:49:22 +0200

    added some styling to the camps view page

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 
-app.set("view engine","ejs");
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs"); //set ejs as the default view engine
+app.use(express.static('public')); //use "public" directory as the assets directory
+app.use(bodyParser.urlencoded({extended:true})); // use body-parser package to parse the post data
 
 var camps = [
     {name:"Bedouin Star" , image:"https://media-cdn.tripadvisor.com/media/photo-m/1280/15/6f/da/a5/bedouin-star.jpg"},
@@ -28,13 +28,17 @@ app.get("/campgrounds/new",function (req,res) {
 });
 
 app.post("/campgrounds",function (req,res) {
+    //extract the form data and push it into the array
     var name = req.body.name;
     var image = req.body.image;
     camps.push({name:name,image: image});
+
+    //go back to the camps 
     res.redirect("/campgrounds");
 });
 
 
 app.listen("3042",function () {
+    //test if the server is working
     console.log("Listening Now");
 });
